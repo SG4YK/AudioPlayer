1fc792ba0775a35890a7c9d242e0f5e2cfe8af84
SG4YK <sg4yk@outlook.com>
2020-05-16 21:40:37 +0800

    Added notification builder

---
+++ app/src/main/java/com/github/sg4yk/audioplayer/utils/NotificationBuilder.kt
@@ -0,0 +1,139 @@
+package com.github.sg4yk.audioplayer.utils
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
+import android.support.v4.media.session.MediaControllerCompat
+import android.support.v4.media.session.MediaSessionCompat
+import android.support.v4.media.session.PlaybackStateCompat
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.media.session.MediaButtonReceiver
+import com.github.sg4yk.audioplayer.R
+import com.github.sg4yk.audioplayer.extensions.isPlayEnabled
+import com.github.sg4yk.audioplayer.extensions.isPlaying
+import com.github.sg4yk.audioplayer.extensions.isSkipToNextEnabled
+import com.github.sg4yk.audioplayer.extensions.isSkipToPreviousEnabled
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+const val NOW_PLAYING_CHANNEL: String = "com.github.sg4yk.AudioPlayer.media.NOW_PLAYING"
+const val NOW_PLAYING_NOTIFICATION: Int = 128
+
+class NotificationBuilder(private val context: Context) {
+    private val notificationManager = NotificationManagerCompat.from(context)
+
+    private val skipToPreviousAction = NotificationCompat.Action(
+        R.drawable.ic_skip_previous_white_24dp,
+        context.getString(R.string.skip_to_previous),
+        MediaButtonReceiver.buildMediaButtonPendingIntent(context, PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS)
+    )
+
+    private val playAction = NotificationCompat.Action(
+        R.drawable.ic_play_arrow_white_24dp,
+        context.getString(R.string.play),
+        MediaButtonReceiver.buildMediaButtonPendingIntent(context, PlaybackStateCompat.ACTION_PLAY)
+    )
+
+    private val pauseAction = NotificationCompat.Action(
+        R.drawable.ic_pause_white_24dp,
+        context.getString(R.string.pause),
+        MediaButtonReceiver.buildMediaButtonPendingIntent(context, PlaybackStateCompat.ACTION_PAUSE)
+    )
+
+    private val skipToNextAction = NotificationCompat.Action(
+        R.drawable.ic_skip_next_white_24dp,
+        context.getString(R.string.skip_to_next),
+        MediaButtonReceiver.buildMediaButtonPendingIntent(context, PlaybackStateCompat.ACTION_SKIP_TO_NEXT)
+    )
+
+    private val stopPendingIntent =
+        MediaButtonReceiver.buildMediaButtonPendingIntent(context, PlaybackStateCompat.ACTION_STOP)
+
+    suspend fun buildNotification(sessionToken: MediaSessionCompat.Token): Notification {
+        if (shouldCreateNowPlayingChannel()) {
+            createNowPlayingChannel()
+        }
+
+        val controller = MediaControllerCompat(context, sessionToken)
+        val description = controller.metadata.description
+        val playbackState = controller.playbackState
+
+        val builder = NotificationCompat.Builder(context, NOW_PLAYING_CHANNEL)
+
+        var playPauseIndex = 0
+        if (playbackState.isSkipToPreviousEnabled) {
+            builder.addAction(skipToPreviousAction)
+            ++playPauseIndex
+        }
+        if (playbackState.isPlaying) {
+            builder.addAction(pauseAction)
+        } else if (playbackState.isPlayEnabled) {
+            builder.addAction(playAction)
+        }
+        if (playbackState.isSkipToNextEnabled) {
+            builder.addAction(skipToNextAction)
+        }
+
+        val mediaStyle = androidx.media.app.NotificationCompat.MediaStyle()
+            .setCancelButtonIntent(stopPendingIntent)
+            .setMediaSession(sessionToken)
+            .setShowActionsInCompactView(playPauseIndex)
+            .setShowCancelButton(true)
+
+        val largeIconBitmap = description.iconUri?.let {
+            resolveUriAsBitmap(it)
+        }
+
+        return builder.setContentIntent(controller.sessionActivity)
+            .setContentText(description.subtitle)
+            .setContentTitle(description.title)
+            .setDeleteIntent(stopPendingIntent)
+            .setLargeIcon(largeIconBitmap)
+            .setOnlyAlertOnce(true)
+            .setSmallIcon(R.drawable.ic_play_circle_filled_white_24dp)
+            .setStyle(mediaStyle)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .build()
+    }
+
+    private fun shouldCreateNowPlayingChannel(): Boolean {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && !nowPlayingChannelExists()
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun nowPlayingChannelExists() =
+        notificationManager.getNotificationChannel(NOW_PLAYING_CHANNEL) != null
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNowPlayingChannel() {
+        val notificationChannel = NotificationChannel(
+            NOW_PLAYING_CHANNEL,
+            "NOW_PLAYING",
+            NotificationManager.IMPORTANCE_LOW
+        )
+            .apply {
+                description = "Empty description"
+            }
+        notificationManager.createNotificationChannel(notificationChannel)
+    }
+
+    private suspend fun resolveUriAsBitmap(uri: Uri): Bitmap? {
+        return withContext(Dispatchers.IO) {
+            val parcelFileDescriptor =
+                context.contentResolver.openFileDescriptor(uri, "r")
+                    ?: return@withContext null
+            val fileDescriptor = parcelFileDescriptor.fileDescriptor
+            BitmapFactory.decodeFileDescriptor(fileDescriptor).apply {
+                parcelFileDescriptor.close()
+            }
+        }
+    }
+}
